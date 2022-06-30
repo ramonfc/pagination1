2f7d1a418b22a47137378b8c0f7e7c6287c32c87
ramon <monchouam@gmail.com>
2022-06-29 19:23:42 -0500

    prueba paginar backend

--- src/App.js
+++ src/App.js
@@ -1,52 +1,81 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import Pagination from './components/Pagination';
-import data from './data/mock-data.json';
+// import data from './data/mock-data.json';
 import './style.scss';
 
 let PageSize = 10;
 
+
 export default function App() {
   const [currentPage, setCurrentPage] = useState(1);
+
+  const url = `http://localhost:8080/api/productos/?limit=${PageSize}&page=${currentPage}`;
+  
+  const [data, setData] = useState([]); 
+  const [isBusy, setBusy] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {          
+            return response.json();
+        }
+        throw new Error('something went wrong while requesting posts');
+      })
+      .then((posts) => {        
+        console.log(posts);
+        setData(posts);
+        setBusy(false);
+      } )
+      .catch((error) => setError(error.message));
+  }, []);
+
 
   const currentTableData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
-    return data.slice(firstPageIndex, lastPageIndex);
+    console.log(data)
+    return data["data"].slice(firstPageIndex, lastPageIndex);
   }, [currentPage]);
 
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>FIRST NAME</th>
-            <th>LAST NAME</th>
-            <th>EMAIL</th>
-            <th>PHONE</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentTableData.map(item => {
-            return (
-              <tr>
-                <td>{item.id}</td>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-                <td>{item.email}</td>
-                <td>{item.phone}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Pagination
-        className="pagination-bar"
-        currentPage={currentPage}
-        totalCount={data.length}
-        pageSize={PageSize}
-        onPageChange={page => setCurrentPage(page)}
-      />
-    </>
-  );
+  return <div> 
+    {!isBusy && !error? 
+    (   
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>id_sku</th>
+              <th>sku</th>
+              <th>title</th>
+              <th>image</th>
+              <th>category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentTableData.map(item => {
+              return (
+                <tr>
+                  <td>{item.id_sku}</td>
+                  <td>{item.sku}</td>
+                  <td>{item.title_to_store}</td>
+                  <td>{item.images_concat}</td>
+                  <td>{item.meli_category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Pagination
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={data.info.countTotal}
+          pageSize={PageSize}
+          onPageChange={page => setCurrentPage(page)}
+        />
+      </>
+    )
+    : (<h1>Sin datos</h1>)} 
+    </div> ;
 }
