08572f3df73ec02bf6fb4df4d359a59d6a886c33
ramon <monchouam@gmail.com>
2022-07-05 12:14:28 -0500

    cambios para que se actualice el color del card al eliminar un producto, asesorado por Migue

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,37 +1,64 @@
-
+import { useEffect, useState } from "react";
 import "./post.scss";
 
 
+// const updateCategory = (id_sku) => {
+//   window.confirm(`Eliminar?`) ? onConfirmUpdate(id_sku) : alert("No eliminado");
+// }
 
-const updateCategory = (id_sku) => {
-  window.confirm(`Eliminar?`) ? onConfirmUpdate(id_sku) : alert("No eliminado");
-}
-
-const onConfirmUpdate = async (id_sku) => {
-  const requestOptions = {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id_sku: id_sku, meli_category: "ELIMINAR" })
-  };
-
-  try {
-    const response = await fetch(`http://localhost:8080/api/productos/change-category`, requestOptions);
-    const data = await response.json();
-    alert(data.affectedRows > 1 ? "Eliminado" : "Error");
-  }
-    catch (error) {
-    alert(error.message)
-  }
-}
 
 
 export default function Post(props) {
 
-  // const [clicked, setClicked] = useState(false);
+  const [flag, setFlag] = useState(false);
+  const [color, setColor] = useState(false);
+ 
+  let currentClass = "post"; 
+  let { id_sku, sku, title_to_store, images_concat, meli_category } = props.data; 
+  
+  
 
-  const { id_sku, sku, title_to_store, images_concat, meli_category } = props.data;
+  const onConfirmUpdate = async (id_sku) => {
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id_sku: id_sku, meli_category: "ELIMINAR" })
+    };
 
-  let currentClass = "post";
+    try {
+      const response = await fetch(`http://localhost:8080/api/productos/change-category`, requestOptions);
+      const data = await response.json();
+      alert(data.affectedRows >= 1 ? "Eliminado" : "Error");
+      props.data.meli_category = "ELIMINAR";
+
+      // if (props.data.meli_category === "ELIMINAR") {
+      //   currentClass += " eliminar";
+      // } else if (props.data.meli_category === "MCO118449") {
+      //   currentClass += " smart-watch";
+      // }
+
+      setColor(true);
+      setFlag(false);
+
+    }
+    catch (error) {
+      alert(error.message)
+    }
+  }
+
+ 
+  useEffect(() => {
+    // document.querySelector("button").addEventListener("click",()=>{console.log("useEffect activado");})
+    if (flag) {
+      console.log("en useEffect");
+    
+      window.confirm(`Eliminar?`) ? onConfirmUpdate(id_sku) : alert("No eliminado");
+
+    }
+  }, [flag]);
+
+  
+
   if (meli_category === "ELIMINAR") {
     currentClass += " eliminar";
   } else if (meli_category === "MCO118449") {
@@ -40,7 +67,7 @@ export default function Post(props) {
 
   return (
     <div className={currentClass} >
-
+      {console.log(currentClass)}
       <small>{sku}</small>
       <h2>{title_to_store}</h2>
 
@@ -48,8 +75,8 @@ export default function Post(props) {
         <img src={images_concat} alt={title_to_store} loading="lazy" />
       </picture>
 
-
-      <button onClick={() => { updateCategory(id_sku) }}>🗑️</button>
+      <button onClick={() => { setFlag(true) }}>🗑️</button>
+      {/* <button onClick={() => { updateCategory(id_sku) }}>🗑️</button> */}
     </div>
   );
 }
